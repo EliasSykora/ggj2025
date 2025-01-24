c48d864e8e795ca09729adeda1f237744a59c8a1
EliasSykora <99739464+EliasSykora@users.noreply.github.com>
2025-01-24 20:27:05 +0100

    Floating, Colliders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,43 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
-    CircleCollider2D myBubble;
+    [SerializeField] CircleCollider2D bubbleCollider;
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float floatSpeed = 5f;
+    [SerializeField] float airAmount = 1f;
+    [SerializeField] float airMinimalAmount = 0.0001f;
+    [SerializeField] GameObject bubbleSprite;
+    private Vector3 scaleChange;
+    private Vector3 pushedAir;
+    private Vector3 minSize;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        myBubble = GetComponent<CircleCollider2D>();
-
+        bubbleCollider = GetComponent<CircleCollider2D>();
+        scaleChange = new Vector3(-0.0005f, -0.0005f, -0.0005f);
+        minSize = new Vector3(0.5f, 0.5f, 0.5f);
+        pushedAir = new Vector3(0.2f, 0.2f, 0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        UpdateAir();
     }
 
+    void UpdateAir()
+    {
+        
+        if (bubbleSprite.transform.localScale.x > minSize.x)
+        {
+            bubbleSprite.transform.localScale += scaleChange;
+        } else { return; }
+        airAmount-= 0.0005f;
+
+    }
 
     void Move()
     {
@@ -45,8 +64,8 @@ public class PlayerMovement : MonoBehaviour
 
         if (value.isPressed)
         {
-            //do stuff
-
+            airAmount += 0.1f;
+            bubbleSprite.transform.localScale += pushedAir;
             myRigidbody.velocity += new Vector2(0f, floatSpeed);
         }
     }
